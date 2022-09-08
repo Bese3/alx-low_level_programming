47840fdffde2af948c0b1b78c591fa2c93a8ed23
Besufikad Yilma <07yilmabese@gmail.com>
2022-09-08 23:33:15 +0300

    fifth c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
  * main - Entry point
  *
-=======
- * *main - Entry point
- * 
->>>>>>> 386901eb02c62ce3fbf9170cc93bf4320913fe6e
  * Return: Always 0 (Success)
  */
 
 int main(void)
-<<<<<<< HEAD
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-=======
-{ 
-	puts ("\"Programming is like building a multilingual puzzle");
-		return (0);
->>>>>>> 386901eb02c62ce3fbf9170cc93bf4320913fe6e
 }
