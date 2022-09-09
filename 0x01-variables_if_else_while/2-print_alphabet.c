57b2bde8eafb7ac26fedcce543939400454ee229
Besufikad Yilma <07yilmabese@gmail.com>
2022-09-09 21:18:46 +0300

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-
-char n;
-
+{ char n;
 for (n = 'a'; n <= 'z'; n++)
- putchar(n);
- putchar('\n');
+putchar(n);
+putchar('\n');
 return (0);
 }
+
