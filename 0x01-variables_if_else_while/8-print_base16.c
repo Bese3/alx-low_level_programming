d6ae5751a0aadbd4356b11a4fa17a9b98007b4d9
Besufikad Yilma <07yilmabese@gmail.com>
2022-09-09 22:06:22 +0300

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,10 @@ int main(void)
 {
 int b;
 char m;
-for (b = 0; b < 10; b++){
-putchar(b + '0');}
-for (m = 'a'; m <= 'f'; m++){
-putchar(m);}
+for (b = 0; b < 10; b++)
+putchar(b + '0');
+for (m = 'a'; m <= 'f'; m++)
+putchar(m);
 putchar('\n');
 return (0);
 }
