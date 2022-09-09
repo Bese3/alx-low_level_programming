76ae926a7ebdd5eeaa00b3976a6ec87463f1c032
Besufikad Yilma <07yilmabese@gmail.com>
2022-09-09 19:56:48 +0300

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-    int a;
-    for (a = 0; a < 10; a++)
-        printf(a);
-        printf('\n');
-    return(0);
+int a;
+for (a = 0; a < 10; a++)
+printf("%d",a);
+printf("\n");
+return(0);
 }
