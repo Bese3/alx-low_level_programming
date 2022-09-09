16e15d6570cad6fdbc950d7c75d01610af0b040d
Besufikad Yilma <07yilmabese@gmail.com>
2022-09-09 19:08:47 +0300

    alphabet 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ char n;
+char m;
+    for (n = 'a'; n <= 'z'; n++)
+        putchar(n);
+    putchar('\n');
+    for (m = 'A'; m <= 'Z'; m++)
+        putchar(m);
+ return(0);
+}
