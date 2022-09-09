a7175e25d7bd0d38aa8368bcbb55bd047fc4c2a6
Besufikad Yilma <07yilmabese@gmail.com>
2022-09-09 18:47:19 +0300

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,8 +14,9 @@ int main(void)
 	for (n = 'a'; n <= 'z'; n++)
 	{
 
-	  putchar(n);
-          putchar('\n');
-	}
+	  putchar(n);}
+       for (n = 'a'; n <= 'z';n++)
+        putchar('\n');
+
 return (0);
 }
