c84463112d92538ca077fd0b08d3f19d481cdebe
besufikad <07yilmabese@gmail.com>
2022-10-12 19:46:24 +0300

    pointer to a function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,16 @@
-#include<stdio.h>
-#include"functions_pointers.h"
-
+#include <stdlib.h>
+#include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
- */
-
-
-
+* print_name - a function that prints a name
+* @name: the name to be printed
+* @f: a pointer to a funtion that accepts a string as
+* parameter and returns nothing
+* Return: returns nothing
+*/
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-return;
-f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
-
