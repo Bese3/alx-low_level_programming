275b427aa6f7942bfb843f7305d5bfe8dcc548bd
besufikad yilma <07yilmabese@gmail.com>
2022-12-12 10:01:44 -0800

    dynamic libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef ALX_LLP_MAIN_H
-#define ALX_LLP_MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include "stddef.h"
 
@@ -24,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* ALX_LLP_MAIN_H */
+#endif /* MAIN_H */
