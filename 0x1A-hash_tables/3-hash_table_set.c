0f1d05a3976510ac4cf97d505454397ee8b6595a
besufikad <07yilmabese@gmail.com>
2023-07-07 18:53:47 +0300

    set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,13 @@
 #include"hash_tables.h"
-
+/**
+ * hash_table_set - adds or updates an element in the hash table
+ * In case of a collision, the new node is added at the beginning of the list
+ * @ht: the hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value){
     hash_node_t* new_hash = (hash_node_t*)malloc(sizeof(hash_node_t));
     unsigned int index;
