428729b318f0c3c519eb65c49b5892764815aa1b
besufika yilma <07yilmabese@gmail.com>
2023-07-07 18:37:17 +0300

    hash_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+
 unsigned long int i;
 hash_table_t *table;
 table = (hash_table_t *) malloc(sizeof(hash_table_t));
 table->size = size;
+if (table == NULL)
+return NULL;
 table->array = (hash_node_t **)malloc(table->size * sizeof(hash_node_t));
+if (!table->array) 
+return NULL;
 for (i = 0; i < size; i++)
 table->array[i] = NULL;
 return (table);
