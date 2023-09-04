fb95092fc6eb48369db161d1d929256022b72d9f
besufikad <07yilmabese@gmail.com>
2023-09-04 10:12:25 +0300

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,32 +6,45 @@
 
 def island_perimeter(grid):
     """Method tha returns the perimeter of the island described in grid."""
-    # p = perimeter, i = iteration 1, j = iteration2
-    p = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+    neighbour_grid = [item[:] for item in grid]
+    y = 0
+    while y < len(grid):
+        z = 0
+        my_grid = grid[y]
+        while z < len(my_grid):
+            neighbour_grid[y][z] = 0
+            z += 1
+        y += 1
+    i = 0
+    while i < len(grid):
+        my_list = grid[i]
+        j = 0
+        while j < len(my_list):
             if grid[i][j] == 1:
                 try:
-                    if grid[i - 1][j] == 0 or i == 0:
-                        p += 1
+                    if my_list[j + 1] == 1:
+                        neighbour_grid[i][j] += 1
                 except IndexError:
-                    p += 1
-
+                    pass
                 try:
-                    if grid[i + 1][j] == 0:
-                        p += 1
+                    if my_list[j - 1] == 1 and j != 0:
+                        neighbour_grid[i][j] += 1
                 except IndexError:
-                    p += 1
-
+                    pass
                 try:
-                    if grid[i][j - 1] == 0 or j == 0:
-                        p += 1
+                    if grid[i + 1][j] == 1:
+                        neighbour_grid[i][j] += 1
                 except IndexError:
-                    p += 1
-
+                    pass
                 try:
-                    if grid[i][j + 1] == 0:
-                        p += 1
+                    if grid[i - 1][j] == 1 and i != 0:
+                        neighbour_grid[i][j] += 1
                 except IndexError:
-                    p += 1
+                    continue
+            j += 1
+        i += 1
+    p = 0
+    for my_grid, my_neighbour in zip(grid, neighbour_grid):
+        for i, j in zip(my_grid, my_neighbour):
+            p += (i * (4 - j))
     return p
